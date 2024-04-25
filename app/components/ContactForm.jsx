d8f2730aa920ff42a40c303123ccb836a63b68fb
Gracie Duncan <gracielduncan@gmail.com>
2024-04-25 11:31:48 -0700

    vercel fix

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -1,30 +1,35 @@
 "use client";
-import { React } from "react";
+import React, {useState} from "react";
 import styles from "../page.module.css"
 import { Button, TextField, TextareaAutosize } from "@mui/material";
 
 
 const ContactForm = () => {
+    const defaultSubmission = {
+        status: null,
+        message: null,
+    }
+
+    const [submission, setSubmission] = useState(defaultSubmission);
 
 
     const handleSubmit = async (formData) => {
-        console.log(formData);
-        let object ={};
+        let object = {};
         formData.forEach((value, key) => object[key] = value);
-        await fetch ('/contact/send', {
+        await fetch('/send', {
             method: 'POST',
             body: JSON.stringify(object),
         })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log("zee response", res);
-            console.log(info)
-        });
+            .then((res) => res.json())
+            .then((res) => {
+                console.log("MY AWESOME RESPONSE", res);
+                setSubmission(res);
+            });
     }
 
     return (
        <div className={styles.formContainer}>
-            <form className={styles.form} action={handleSubmit}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <TextField className={styles.textField} name="name" id="outlined-basic" label="Name" variant="filled" />
                 <TextField className={styles.textField} name="email" id="outlined-basic" label="Email" variant="filled" />
                 <TextareaAutosize className={styles.textField} id="message" label="Message" minRows={3} />
--- app/send/route.js
+++ app/send/route.js
@@ -1,30 +1,39 @@
-import { NextResponse } from "next/server";
-
-
 const nodemailer = require("nodemailer");
+import { NextResponse } from 'next/server';
 
 const transporter = nodemailer.createTransport({
     host: process.env.NODEMAILER_HOST || "smtp.gmail.com",
     port: process.env.NODEMAILER_PORT || 465,
-    secure: process.env.NODEMAILER_SECURE || true, 
+    secure: process.env.NODEMAILER_SECURE || true, // Use `true` for port 465, `false` for all other ports
     auth: {
-        user: process.env.NODEMAILER_USERNAME || '',
-        pass: process.env.NODEMAILER_PASSWORD || ''
+        user: process.env.NODEMAILER_USERNAME,
+        pass: process.env.NODEMAILER_PASSWORD,
     },
 });
 
 
 export async function POST(request) {
-    const body = await request.json();
-    const info = await transporter.sendMail({
-        from: 'gracielduncan@gmail.com', // sender address
-        to: "gduncan@nic.edu", // list of receivers
-        subject: `Vercel Contact ${body.name}`, // Subject line
-        text: body.message, // plain text body
-        html: `<b>${body.message}</b>`, // html body
-    });
-    const response = {};
-    console.log(body);
-    return NextResponse.json(response); 
-    
+    try {
+        const body = await request.json();
+        console.log("SERVERS BODY", body);
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: 'gracielduncan@gmail.com', // sender address
+            to: "gduncan@nic.edu", // list of receivers
+            subject: `Vercel Contact ${body.name}`, // Subject line
+            text: body.message, // plain text body
+            html: `<b>${body.message}</b>`, // html body
+        });
+        return NextResponse.json({
+            status: "success",
+            message: "Thank you for contacting me, I will response ASAP."
+        });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            status: "error",
+            message: "Oops, something went wrong."
+        });
+    }
+
 }
